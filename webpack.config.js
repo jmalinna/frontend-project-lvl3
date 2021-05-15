5a4e48439313756fcd77ac5fd0a82cda6f8f0ef8
Julia Malinina <malinina.artwork@gmail.com>
2021-05-16 00:45:51 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './index.js',
   output: {
     filename: 'index_bundle.js',
