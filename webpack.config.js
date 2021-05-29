1ee1653a04e7e38fdc7611e6f725e98eef57eba5
Julia Malinina <malinina.artwork@gmail.com>
2021-05-29 20:56:59 +0300

    update package.json

--- webpack.config.js
+++ webpack.config.js
@@ -44,14 +44,6 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
         test: /\.s[ac]ss$/i,
         use: [
           // Creates `style` nodes from JS strings
