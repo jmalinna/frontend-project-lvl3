9272d363bd8333cd64b6be2e5a4528b64b7e4863
Julia Malinina <malinina.artwork@gmail.com>
2021-06-23 18:21:46 +0300

    update UpdatePosts.js

--- src/updatePosts.js
+++ src/updatePosts.js
@@ -5,30 +5,29 @@ import addPostsToState from './addPosts.js';
 import addProxy from './addProxy.js';
 
 const addNewRssPosts = (watchedState) => {
-  const updatePosts = () => {
-    watchedState.feeds.forEach((feed) => {
-      const proxy = addProxy(feed.link);
-      const { id } = feed;
-      axios.get(proxy)
-        .then((response) => parseRSS(response.data.contents))
-        .then((data) => differenceBy(data.items, watchedState.posts, 'link'))
-        .then((newPosts) => {
-          if (newPosts) {
-            watchedState.updatedPosts = [];
+  const updatePosts = (feed) => {
+    const proxy = addProxy(feed.link);
+    const { id } = feed;
 
-            addPostsToState(id, { items: newPosts }, 'updatedPosts', watchedState);
+    axios.get(proxy)
+      .then((response) => parseRSS(response.data.contents))
+      .then((data) => differenceBy(data.items, watchedState.posts, 'link'))
+      .then((newPosts) => {
+        if (newPosts) {
+          watchedState.updatedPosts = [];
 
-            watchedState.updatedPosts.forEach((post) => {
-              watchedState.posts.push(post);
-            });
-          }
-        })
-        .catch(() => setTimeout(() => addNewRssPosts(watchedState), 5000));
-    });
-    return Promise.resolve();
+          addPostsToState(id, { items: newPosts }, 'updatedPosts', watchedState);
+
+          watchedState.updatedPosts.forEach((post) => {
+            watchedState.posts.push(post);
+          });
+        }
+      });
   };
 
-  const promise = updatePosts();
-  promise.then(() => setTimeout(() => addNewRssPosts(watchedState), 5000));
+  const promises = watchedState.feeds.map((feed) => updatePosts(feed));
+
+  const promise = Promise.all(promises);
+  promise.finally(() => setTimeout(() => addNewRssPosts(watchedState), 5000));
 };
 export default addNewRssPosts;
